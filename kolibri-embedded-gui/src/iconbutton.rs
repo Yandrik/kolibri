16debc51a6a5a147f80b8a27844b7e1514d8648f
Yandrik <me@yandrik.dev>
2024-11-20 22:29:43 +0100

    feat(iconbutton): can now add label under icon

--- kolibri-embedded-gui/src/iconbutton.rs
+++ kolibri-embedded-gui/src/iconbutton.rs
@@ -5,13 +5,16 @@ use core::marker::PhantomData;
 use embedded_graphics::draw_target::DrawTarget;
 use embedded_graphics::geometry::{Point, Size};
 use embedded_graphics::image::Image;
+use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::pixelcolor::PixelColor;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::{PrimitiveStyleBuilder, Rectangle};
+use embedded_graphics::text::{Alignment, Baseline, Text};
 use embedded_iconoir::prelude::{IconoirIcon, IconoirNewIcon};
 
 pub struct IconButton<'a, ICON: IconoirIcon> {
     icon: PhantomData<ICON>,
+    label: Option<&'a str>,
     smartstate: Container<'a, Smartstate>,
 }
 
@@ -20,13 +23,20 @@ impl<'a, ICON: IconoirIcon> IconButton<'a, ICON> {
         Self {
             icon: PhantomData,
             smartstate: Container::empty(),
+            label: None,
         }
+    }
+
+    pub fn label(mut self, label: &'a str) -> Self {
+        self.label = Some(label);
+        self
     }
 
     pub fn new_from_type() -> Self {
         Self {
             icon: PhantomData,
             smartstate: Container::empty(),
+            label: None,
         }
     }
 
@@ -47,12 +57,29 @@ impl<ICON: IconoirIcon> Widget for IconButton<'_, ICON> {
         let padding = ui.style().spacing.button_padding;
         let border = ui.style().border_width;
 
-        let height = max(
+        let mut height = max(
             max(ui.style().default_widget_height, ui.get_row_height()),
             icon.bounding_box().size.height + 2 * padding.height + 2 * border,
         );
 
-        let width = height;
+        let mut width = height;
+
+        let font = ui.style().default_font;
+
+        let mut text = if let Some(label) = self.label {
+            let mut text = Text::new(
+                label,
+                Point::new(0, 0),
+                MonoTextStyle::new(&font, ui.style().text_color),
+            );
+            text.text_style.alignment = Alignment::Center;
+            text.text_style.baseline = Baseline::Top;
+            height += padding.height + text.bounding_box().size.height;
+            width = width.max(text.bounding_box().size.width + 2 * padding.width + 2 * border);
+            Some(text)
+        } else {
+            None
+        };
 
         let size = Size::new(width, height);
 
@@ -75,10 +102,28 @@ impl<ICON: IconoirIcon> Widget for IconButton<'_, ICON> {
         let center_offset = iresponse.area.top_left
             + Point::new(
                 ((iresponse.area.size.width - size.size.width) / 2) as i32,
-                ((iresponse.area.size.height - size.size.height) / 2) as i32,
+                ((iresponse.area.size.height
+                    - size.size.height
+                    - text
+                        .map(|t| t.bounding_box().size.height + padding.height)
+                        .unwrap_or(0))
+                    / 2) as i32,
             );
 
         let icon_img = Image::new(&icon, center_offset);
+
+        // center text (if it exists)
+        if let Some(text) = text.as_mut() {
+            let center_offset = iresponse.area.top_left
+                + Point::new(
+                    (iresponse.area.size.width / 2) as i32,
+                    (iresponse.area.size.height
+                        - text.bounding_box().size.height
+                        - padding.height
+                        - border) as i32,
+                );
+            text.translate_mut(center_offset);
+        }
 
         // check for click
         let click = matches!(iresponse.interaction, Interaction::Release(_));
@@ -129,6 +174,9 @@ impl<ICON: IconoirIcon> Widget for IconButton<'_, ICON> {
             )
             .ok();
             ui.draw(&icon_img).ok();
+            if let Some(text) = text.as_mut() {
+                ui.draw(text).unwrap();
+            }
 
             ui.finalize()?;
         }
