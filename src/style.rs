57b066b75ba2d91a3ada29d19faf23a3bf2c2a80
Yandrik <me@yandrik.dev>
2025-07-25 22:50:18 +0200

    feat(style): adjust styles for better fit with rounded corners

--- src/style.rs
+++ src/style.rs
@@ -116,7 +116,7 @@ pub fn medsize_rgb565_style() -> Style<Rgb565> {
         default_font: mono_font::iso_8859_10::FONT_9X15,
         spacing: Spacing {
             item_spacing: Size::new(8, 4),
-            button_padding: Size::new(5, 5),
+            button_padding: Size::new(6, 5),
             default_padding: Size::new(1, 1),
             window_border_padding: Size::new(3, 3),
         },
@@ -144,7 +144,7 @@ pub fn medsize_light_rgb565_style() -> Style<Rgb565> {
         default_font: mono_font::iso_8859_10::FONT_9X15,
         spacing: Spacing {
             item_spacing: Size::new(8, 4),
-            button_padding: Size::new(5, 5),
+            button_padding: Size::new(6, 5),
             default_padding: Size::new(1, 1),
             window_border_padding: Size::new(3, 3),
         },
@@ -172,7 +172,7 @@ pub fn medsize_sakura_rgb565_style() -> Style<Rgb565> {
         default_font: mono_font::ascii::FONT_9X15,
         spacing: Spacing {
             item_spacing: Size::new(8, 4),
-            button_padding: Size::new(5, 5),
+            button_padding: Size::new(6, 5),
             default_padding: Size::new(1, 1),
             window_border_padding: Size::new(3, 3),
         },
@@ -200,7 +200,7 @@ pub fn medsize_blue_rgb565_style() -> Style<Rgb565> {
         default_font: mono_font::iso_8859_10::FONT_9X15,
         spacing: Spacing {
             item_spacing: Size::new(8, 4),
-            button_padding: Size::new(5, 5),
+            button_padding: Size::new(6, 5),
             default_padding: Size::new(1, 1),
             window_border_padding: Size::new(3, 3),
         },
@@ -232,7 +232,7 @@ pub fn medsize_crt_rgb565_style() -> Style<Rgb565> {
             default_padding: Size::new(1, 1),
             window_border_padding: Size::new(3, 3),
         },
-        corner_radius: 8,
+        corner_radius: 0,
     }
 }
 
@@ -260,7 +260,7 @@ pub fn medsize_retro_rgb565_style() -> Style<Rgb565> {
             default_padding: Size::new(1, 1),
             window_border_padding: Size::new(3, 3),
         },
-        corner_radius: 8,
+        corner_radius: 0,
     }
 }
 
@@ -310,7 +310,7 @@ pub struct Style<COL: PixelColor> {
     pub border_color: COL,
     /// Primary accent color for interactive elements
     pub primary_color: COL,
-    /// Secondary accent color for additional highlighting
+    /// Secondary accent color for additional highlighting;
     pub secondary_color: COL,
     /// Color used for icons
     pub icon_color: COL,
