fe560acdd108c2a791a73932c8a832f75565056f
Merisy-Thing <merity-thing@outlook.com>
2025-11-13 15:44:59 +0800

    add example

---
+++ examples/combo_box_example.rs
@@ -0,0 +1,143 @@
+use embedded_graphics::geometry::Size;
+use embedded_graphics::mono_font::ascii;
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::{Point, WebColors};
+use embedded_graphics_simulator::sdl2::MouseButton;
+use embedded_graphics_simulator::{
+    OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
+};
+use kolibri_embedded_gui::checkbox::Checkbox;
+use kolibri_embedded_gui::combo_box::ComboBox;
+use kolibri_embedded_gui::label::Label;
+use kolibri_embedded_gui::ui::{Interaction, PopupState, Ui};
+
+fn main() -> Result<(), core::convert::Infallible> {
+    // Simulator Setup (ILI9341-like Display)
+    let mut display = SimulatorDisplay::<Rgb565>::new(Size::new(320, 240));
+
+    // Output Settings. Change for other screen appearance / size scaling.
+    let output_settings = OutputSettingsBuilder::new()
+        // .pixel_spacing(2)
+        .scale(1)
+        .build();
+    let mut window = Window::new("Hello World", &output_settings);
+
+    // input handling variables
+    let mut mouse_down = false;
+    let mut last_down = false;
+    let mut location = Point::new(0, 0);
+
+    // counter for incrementing thingy
+    let mut popup_state = PopupState::default();
+
+    println!("Hello World!");
+
+    let mut checked = false;
+    let mut selected0 = "Hello";
+    let mut selected1 = "World";
+
+    'outer: loop {
+        let mut popup_buffer = [Rgb565::CSS_BLACK; 320 * 240];
+
+        // create UI (needs to be done each frame)
+        let mut ui = Ui::new_fullscreen(
+            &mut display,
+            kolibri_embedded_gui::style::medsize_light_rgb565_style(),
+        );
+        //ui.draw_widget_bounds_debug(Rgb565::RED);
+
+        // handle input
+        match (last_down, mouse_down, location) {
+            (false, true, loc) => {
+                ui.interact(Interaction::Click(loc));
+            }
+            (true, true, loc) => {
+                ui.interact(Interaction::Drag(loc));
+            }
+            (true, false, loc) => {
+                ui.interact(Interaction::Release(loc));
+            }
+            (false, false, loc) => {
+                ui.interact(Interaction::Hover(loc));
+            }
+        }
+
+        // clear UI background (for non-incremental redrawing framebuffered applications)
+        ui.clear_background().ok();
+        ui.begin_popup(&mut popup_state, &mut popup_buffer);
+
+        last_down = mouse_down;
+
+        // === ACTUAL UI CODE STARTS HERE ===
+
+        ui.add(Label::new("ComboBox Example").with_font(ascii::FONT_10X20));
+
+        if ComboBox::new()
+            .selected_text(selected0)
+            .show_ui(&mut ui, || {
+                ComboBox::new_contents(
+                    &mut selected0,
+                    &["Hello 1", "Hello Hello 2", "Hello Hello Hello 3"],
+                )
+            })
+            .changed()
+        {
+            println!("ComboBox changed to {}", selected0);
+        }
+
+        ui.add_horizontal(Checkbox::new(&mut checked));
+
+        if ComboBox::new()
+            .selected_text(selected1)
+            .with_width(100)
+            .show_ui(&mut ui, || {
+                ComboBox::new_contents(
+                    &mut selected1,
+                    &["World 1", "World World 2", "World World World 3"],
+                )
+            })
+            .changed()
+        {
+            println!("ComboBox changed to {}", selected1);
+        }
+        ui.new_row();
+        ui.add(Label::new("Label 1"));
+        ui.add(Label::new("Label 2"));
+        ui.add(Label::new("Long Label 3"));
+        ui.add(Label::new("Long Long Label 4"));
+        ui.add(Label::new("Long Long Long Label 5"));
+
+        ui.end_popup();
+        // === ACTUAL UI CODE ENDS HERE ===
+
+        // simulator window update
+        window.update(&display);
+
+        // take input, and quit application if necessary
+        for evt in window.events() {
+            match evt {
+                SimulatorEvent::KeyUp { .. } => {}
+                SimulatorEvent::KeyDown { .. } => {}
+                SimulatorEvent::MouseButtonUp { mouse_btn, point } => {
+                    if let MouseButton::Left = mouse_btn {
+                        mouse_down = false;
+                    }
+                    location = point;
+                }
+                SimulatorEvent::MouseButtonDown { mouse_btn, point } => {
+                    if let MouseButton::Left = mouse_btn {
+                        mouse_down = true;
+                    }
+                    location = point;
+                }
+                SimulatorEvent::MouseWheel { .. } => {}
+                SimulatorEvent::MouseMove { point } => {
+                    location = point;
+                }
+                SimulatorEvent::Quit => break 'outer,
+            }
+        }
+        std::thread::sleep(std::time::Duration::from_millis(20));
+    }
+    Ok(())
+}
